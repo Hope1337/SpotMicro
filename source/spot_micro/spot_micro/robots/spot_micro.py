81963676fb7cd3c96f951261f9aa3441c12a345c
manh <eguitarsml@gmail.com>
2025-11-25 13:59:01 +0700

    init spot_micro

---
+++ source/spot_micro/spot_micro/robots/spot_micro.py
@@ -0,0 +1,80 @@
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ActuatorNetLSTMCfg, DCMotorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+
+from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+from isaaclab.actuators import ImplicitActuatorCfg, IdealPDActuatorCfg
+
+joints = [
+    'front_left_shoulder',
+    'front_right_shoulder',
+    'rear_left_shoulder',
+    'rear_right_shoulder',
+    'front_left_leg',
+    'front_left_foot',
+    'front_right_leg',
+    'front_right_foot',
+    'rear_left_leg',
+    'rear_left_foot',
+    'rear_right_leg',
+    'rear_right_foot'
+]
+
+import os
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+USD_PATH = os.path.join(CURRENT_DIR, "spot_micro.usd")
+
+SPOT_MICRO_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=USD_PATH,
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, .2),  
+        joint_pos={
+            ".*_left_shoulder": 0.1,    
+            ".*_right_shoulder": -0.1,       
+            "front_left_leg":  0.9,             
+            "front_right_leg": 0.9,
+            "rear_left_leg":  1.1,
+            "rear_right_leg": 1.1,
+            ".*_foot": -1.5,             
+        },
+        joint_vel={".*": 0.0}, 
+    ),
+    
+    actuators={
+        "servos": DCMotorCfg(
+            joint_names_expr=joints,
+            effort_limit=33.5,
+            saturation_effort=33.5,
+            velocity_limit=21.0,
+            stiffness=25.0,
+            damping=0.5,
+            friction=0.0,
+        ),
+    }
+    #actuators={
+        #"servos": ImplicitActuatorCfg(
+            #joint_names_expr=joints,
+            ##velocity_limit=8.0,
+            #effort_limit=45.,
+            #stiffness=20.,  
+            #damping=0.5,    
+            #armature=0.0
+        #),
+    #}
+)
